b0462bcd9b56df14898e59acbf0ca71d85555a7c
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-04 16:07:55 +0100

    added aircraft getters and setters

--- app/aircraft_class.py
+++ app/aircraft_class.py
@@ -1,2 +1,16 @@
 class Aircraft:
-    pass
+    def __init__(self, name, capacity):
+        self.__name = name
+        self.__capacity = capacity
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def set_capacity(self, capacity):
+        self.__capacity = capacity
--- tests/aircraft_test.py
+++ tests/aircraft_test.py
@@ -3,4 +3,15 @@ from app.aircraft_class import *
 
 
 class AircraftTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.aircraft_1 = Aircraft('Airbus A380', 200)
+
+    def test_get_name(self):
+        self.assertEqual(self.aircraft_1.get_name(), 'Airbus A380')
+        self.aircraft_1.set_name('Boeing 767')
+        self.assertEqual(self.aircraft_1.get_name(), 'Boeing 767')
+
+    def test_get_capacity(self):
+        self.assertEqual(self.aircraft_1.get_capacity(), 200)
+        self.aircraft_1.set_capacity(250)
+        self.assertEqual(self.aircraft_1.get_capacity(), 250)
