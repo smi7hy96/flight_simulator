0a166bc3c3fc4d0367beaf81539c6f364ac5ce5b
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-04 18:27:06 +0100

    added produce list of passengers and basic password protection for staff methods

--- app/flight_trip_class.py
+++ app/flight_trip_class.py
@@ -33,8 +33,11 @@ class FlightTrip:
     def get_plane(self):
         return self.__plane
 
-    def set_plane(self, plane):
-        self.__plane = plane
+    def set_plane(self, plane, password):
+        if password == 'adminplane':
+            self.__plane = plane
+        else:
+            return 'wrong password, change denied'
 
     def get_passenger_list(self):
         return self.__passenger_list
@@ -75,6 +78,27 @@ class FlightTrip:
                 return True
         return False
 
+    def produce_list_of_passengers(self, password):
+        if password == 'adminplane':
+            names = self.__get_all_passenger_names()
+            passport_nos = self.__get_all_passenger_passports()
+            for x in range(len(names)):
+                names[x] = names[x] + " : " + passport_nos[x]
+            return '\n'.join(names)
+        else:
+            return 'wrong password, change denied'
+
+    def __get_all_passenger_names(self):
+        names_list = []
+        for name in self.get_passenger_list()['passengers']:
+            names_list.append(name.get_name())
+        return names_list
+
+    def __get_all_passenger_passports(self):
+        passport_list = []
+        for passport in self.get_passenger_list()['passengers']:
+            passport_list.append(passport.get_passport_no())
+        return passport_list
 
     def __count_people_in_flight(self, all_staff, all_passengers):
         count_total = 0
--- tests/flight_trip_test.py
+++ tests/flight_trip_test.py
@@ -38,8 +38,9 @@ class FlightTripTest(unittest.TestCase):
     def test_get_plane(self):
         plane_2 = Plane('Boeing 767', 250)
         self.assertEqual(self.flight_trip_1.get_plane().get_name(), 'Airbus A380')
-        self.flight_trip_1.set_plane(plane_2)
+        self.flight_trip_1.set_plane(plane_2, 'adminplane')
         self.assertEqual(self.flight_trip_1.get_plane().get_name(), 'Boeing 767')
+        self.assertEqual(self.flight_trip_1.set_plane(plane_2, 'password'), 'wrong password, change denied')
 
     def test_add_passenger(self):
         airport_1 = Airport('Madrid')
@@ -56,7 +57,7 @@ class FlightTripTest(unittest.TestCase):
         staff_1 = Staff('ABC123', 'Susan', 1, airport_1.get_name())
         plane_2 = Plane('Airbus1', 0)
         pass_list = {'staff': [staff_1], 'passengers': []}
-        self.flight_trip_1.set_plane(plane_2)
+        self.flight_trip_1.set_plane(plane_2, 'adminplane')
         self.assertEqual(self.flight_trip_1.add_passenger_to_flight(staff_1), 'flight full')
         self.assertEqual(self.flight_trip_1.set_passenger_list(pass_list), 'too many people in list')
 
@@ -84,3 +85,9 @@ class FlightTripTest(unittest.TestCase):
         passenger_1 = Passenger('DEF456', 'Peter', '11112222LL')
         self.flight_trip_1.add_passenger_to_flight(staff_1)
         self.assertEqual(self.flight_trip_1.remove_passenger_from_flight(passenger_1), 'passenger not on flight')
+
+    def test_produce_list(self):
+        passenger_1 = Passenger('DEF456', 'Peter', '11112222LL')
+        self.flight_trip_1.add_passenger_to_flight(passenger_1)
+        self.assertEqual(self.flight_trip_1.produce_list_of_passengers('adminplane'), 'Peter : 11112222LL')
+
